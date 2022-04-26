0d46368ff37a88dee6e9ceba48d835fa87591aae
Nicolae Racovita <nicolae.racovita@email.com>
2022-04-26 16:02:55 -0400

    initial commit for UI

---
+++ ui/pages/admin.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const AboutPage = () => (
+  <Layout title="Admin | TaxMe">
+    <h1>Admin Dashboard</h1>
+
+    
+  </Layout>
+)
+
+export default AboutPage
